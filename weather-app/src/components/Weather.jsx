278de6e0f20e573f2837f79c94a91b24c3493993
Your Name <asmitastha2001@gmail.com>
2024-05-03 07:44:04 +0545

    completed normal function

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -11,14 +11,8 @@ const Weather = () => {
   const [selectedCountry, setSelectedCountry] = useState({});
   const [selectedCity, setSelectedCity] = useState([]);
   const [weatherDetails, setWeatherDetails] = useState([]);
-  const [countryInput, setCountryInput] = useState('')
-  const [cityInput, setCityInput] = useState('')
-  const toggleUnit = () => {
-    setUnit(unit === "F" ? "C" : "F");
-  };
 
   useEffect(() => {
-    // Use Country.getAllCountries() to get all countries
     setAllCountries(
       Country.getAllCountries().map((country) => ({
         value: {
@@ -31,13 +25,29 @@ const Weather = () => {
     );
   }, []);
 
-  console.log(allCountries);
+  useEffect(() => {
+    const getWeatherDetails = async () => {
+      if (
+        selectedCity &&
+        selectedCity.value &&
+        selectedCity.value.latitude &&
+        selectedCity.value.longitude
+      ) {
+        const fetchWeatherDetails = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${selectedCity.value.latitude}&longitude=${selectedCity.value.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,snowfall,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,wind_speed_10m_max,wind_gusts_10m_max&wind_speed_unit=mph&timezone=GMT`
+        );
+
+        const data = await fetchWeatherDetails.json();
+        setWeatherDetails(data);
+      }
+    };
+    getWeatherDetails();
+  }, [selectedCity]);
 
   const handleSelectedCountry = (option) => {
     setSelectedCountry(option);
     setSelectedCity(null);
   };
-
   const handleSelectedCity = (option) => {
     // Parse latitude and longitude to integers
     const parsedCity = {
@@ -50,28 +60,8 @@ const Weather = () => {
     setSelectedCity(parsedCity);
   };
 
-
-  // const handleSelectedCountry = (event) => {
-  //   setCountryInput(event.target.value);
-  //   setSelectedCountry(allCountries.find(country => country.label === event.target.value));
-  //   setSelectedCity(null);
-  // };
-
-  // const handleSelectedCity = (event) => {
-  //   setCityInput(event.target.value);
-  //   setSelectedCity(event.target.value);
-  // };
-
-  const getWeatherDetails = async (e) => {
-    e.preventDefault();
-    const fetchWeatherDetails = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${selectedCity?.value?.latitude}&longitude=${selectedCity?.value?.longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,snowfall,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,wind_speed_10m_max,wind_gusts_10m_max&wind_speed_unit=mph&timezone=GMT`
-    );
-  
-    const data = await fetchWeatherDetails.json();
-    setWeatherDetails(data);
-  };
   console.log("Data weather", weatherDetails);
+
   return (
     <div className="bg-cyan-500 min-h-screen flex justify-center">
       <div className="container bg-white flex justify-center relative top-16">
@@ -83,7 +73,7 @@ const Weather = () => {
                 options={allCountries}
                 value={selectedCountry}
                 onChange={handleSelectedCountry}
-                className="custom-select border-none outline-none text-black font-bold text-xl"
+                className="custom-select text-black font-bold text-xl w-32"
                 style={{ marginRight: "5px" }}
               />
               <Select
@@ -98,20 +88,13 @@ const Weather = () => {
                 }))}
                 value={selectedCity}
                 onChange={handleSelectedCity}
-                className="custom-select"
+                className="custom-select  text-black font-bold text-xl w-32"
                 style={{ marginRight: "5px" }}
               />
-              {/* <hr /> */}
-              <button
-                onClick={getWeatherDetails}
-                className="bg-blue-400 p-4 rounded"
-              >
-                Go
-              </button>
             </div>
             <p className="text-gray-600 ml-1">, it's mostly cloudy</p>
           </div>
-          <div className="flex justify-between mt-9 px-20 md:px-[10px]">
+          <div className="flex justify-between mt-16 px-20 md:px-[10px]">
             <div>
               <img src={Mix} alt="mix" className="w-20 rounded-full" />
             </div>
@@ -119,40 +102,37 @@ const Weather = () => {
               <p className="text-gray-950 text-[40px] ">
                 {weatherDetails?.daily?.temperature_2m_max[0]}
               </p>
-              <p className="text-gray-600 text-sm">
+              <p className="text-gray-500 text-sm">
                 {selectedCity?.value?.latitude} °| {""}
                 {selectedCity?.value?.longitude} °
               </p>
-              <h3>
-                {selectedCountry?.label},{selectedCity?.label}
-              </h3>
             </div>
-            <div className="text-gray-600">
+            <div className="text-gray-500">
               <div className="flex">
                 <p className="mt-1 pr-2">
-                  <CiTempHigh />
+                  <CiTempHigh className="text-[20px]"/>
                 </p>
-                <p>
+                <p className="font-semibold text-xl">
                   {weatherDetails?.daily?.wind_speed_10m_max[0]}{" "}
                   <span className="text-[12px]">mph</span>
                 </p>
               </div>
               <div className="flex">
                 <p className="mt-1 pr-2">
-                  <CiUmbrella />
+                  <CiUmbrella className="text-[20px]"/>
                 </p>
-                <p>
+                <p className="font-semibold text-xl">
                   {weatherDetails?.current?.rain}{" "}
                   <span className="text-[12px]">%</span>
                 </p>
               </div>
               <div className="flex">
                 <p className="mt-1 pr-2">
-                  <CiDroplet />
+                  <CiDroplet className="text[20px]"/>
                 </p>
-                <p>
+                <p className="font-semibold text-xl">
                   {weatherDetails?.current?.relative_humidity_2m}
-                  <span className="text-[12px]">%</span>
+                  <span className="text-[12px]"> %</span>
                 </p>
               </div>
             </div>
@@ -163,12 +143,20 @@ const Weather = () => {
                 <img
                   src={Mix}
                   alt="mix"
-                  className="w-7 rounded-full mb-3  ml-2"
+                  className="w-7 rounded-full mb-3  ml-5"
                 />
-                <div className="text-gray-600">
-                <p className="text-xl ">{Math.round(weatherDetails?.daily?.apparent_temperature_max[3])}°/{' '}
-                {Math.round(weatherDetails?.daily?.apparent_temperature_min[3])}°</p>
-                  <p className="text-[12px] mt-2 ml-2">Wed</p>
+                <div className="text-gray-500">
+                  <p className="text-xl ">
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_max[3]
+                    )}
+                    °/{" "}
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_min[3]
+                    )}
+                    °
+                  </p>
+                  <p className="text-[12px] mt-2 ml-5">Wed</p>
                 </div>
               </div>
 
@@ -176,12 +164,20 @@ const Weather = () => {
                 <img
                   src={Mix}
                   alt="mix"
-                  className="w-7 rounded-full mb-3  ml-2"
+                  className="w-7 rounded-full mb-3  ml-5"
                 />
-                <div className="text-gray-600">
-                <p className="text-xl ">{Math.round(weatherDetails?.daily?.apparent_temperature_max[4])}°/{' '}
-                {Math.round(weatherDetails?.daily?.apparent_temperature_min[4])}°</p>
-                  <p className="text-[12px] mt-2 ml-2">Thurs</p>
+                <div className="text-gray-500">
+                  <p className="text-xl ">
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_max[4]
+                    )}
+                    °/{" "}
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_min[4]
+                    )}
+                    °
+                  </p>
+                  <p className="text-[12px] mt-2 ml-5">Thurs</p>
                 </div>
               </div>
 
@@ -189,12 +185,20 @@ const Weather = () => {
                 <img
                   src={Mix}
                   alt="mix"
-                  className="w-7 rounded-full mb-3  ml-2"
+                  className="w-7 rounded-full mb-3  ml-5"
                 />
-                <div className="text-gray-600">
-                <p className="text-xl ">{Math.round(weatherDetails?.daily?.apparent_temperature_max[5])}°/ {""}
-                {Math.round(weatherDetails?.daily?.apparent_temperature_min[5])}°</p>
-                  <p className="text-[12px] mt-2 ml-2">Fri</p>
+                <div className="text-gray-500">
+                  <p className="text-xl ">
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_max[5]
+                    )}
+                    °/ {""}
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_min[5]
+                    )}
+                    °
+                  </p>
+                  <p className="text-[12px] mt-2 ml-5">Fri</p>
                 </div>
               </div>
 
@@ -202,31 +206,23 @@ const Weather = () => {
                 <img
                   src={Mix}
                   alt="mix"
-                  className="w-7 rounded-full mb-3  ml-2"
+                  className="w-7 rounded-full mb-3  ml-5"
                 />
-                <div className="text-gray-600">
-                <p className="text-xl ">{Math.round(weatherDetails?.daily?.apparent_temperature_max[6])}°/ {""}
-                {Math.round(weatherDetails?.daily?.apparent_temperature_min[6])}°</p>
-                  <p className="text-[12px] mt-2 ml-2">Sat</p>
+                <div className="text-gray-500">
+                  <p className="text-xl ">
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_max[6]
+                    )}
+                    °/ {""}
+                    {Math.round(
+                      weatherDetails?.daily?.apparent_temperature_min[6]
+                    )}
+                    °
+                  </p>
+                  <p className="text-[12px] mt-2 ml-5">Sat</p>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="flex justify-center mt-7">
-            <button
-              onClick={toggleUnit}
-              className={`text-gray-600 ${unit === "F" ? "text-black" : ""}`}
-            >
-              F
-            </button>
-            <span className="mx-1">|</span>
-            <button
-              onClick={toggleUnit}
-              className={`text-gray-600 ${unit === "C" ? "text-black" : ""}`}
-            >
-              C
-            </button>
           </div>
         </div>
       </div>
